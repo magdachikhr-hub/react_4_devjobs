9c42013200d92750f280e9020dadcde5525394a9
Magda Chikhradze <magda292005@gmail.com>
2026-07-22 14:54:44 +0400

    useEffect

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Test() {
   const [count, setCount] = useState(0);
-
+  const [decrease, setDecrease] = useState(100);
   const handleCount = () => {
     setCount((c) => c + 1);
     console.log(count);
   };
+  const handleDecrease = () => {
+    setDecrease((d) => d - 1);
+    console.log(decrease);
+  };
+
+  useEffect(() => {
+    console.log("hello");
+    return () => {
+      console.log("stopped");
+    };
+  }, [decrease]);
 
   return (
     <>
       <button onClick={handleCount}>add 1</button>
+      <div>{count}</div>
+
+      <button onClick={handleDecrease}>decrease by 1</button>
+      <div>{decrease}</div>
     </>
   );
 }
