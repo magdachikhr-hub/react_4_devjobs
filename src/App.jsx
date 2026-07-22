88092ed0aa6838800372b9d060db9c60dab4b1dc
Magda Chikhradze <magda292005@gmail.com>
2026-07-22 15:29:25 +0400

    DarkMode

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Jobs from "./pages/Jobs";
 import JobDetails from "./pages/JobDetails";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-import Test from "./components/Test";
+// import Test from "./components/Test";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
+      <Header></Header>
       <main className="bg-[#F4F6F8] min-h-screen pl-6 pr-6 sm:pl-10 sm:pr-10">
         <Routes>
           <Route path="/" element={<Jobs></Jobs>}></Route>
@@ -18,7 +20,7 @@ export default function App() {
         </Routes>
       </main>
       <Footer></Footer>
-      <Test></Test>
+      {/* <Test></Test> */}
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
-  return <div>Header</div>;
+  const [isDark, setIsDark] = useState(() => {
+    return localStorage.getItem("theme");
+  });
+
+  // const handleDarkMode = () => {
+  //   setIsDark((d) => !d);
+  //   console.log(isDark);
+  // };
+
+  const handleDarkMode = () => {
+    setIsDark((d) => !d);
+    if (isDark) {
+      localStorage.setItem("theme", "#000");
+    } else {
+      localStorage.setItem("theme", "#FFF");
+    }
+  };
+  console.log(isDark);
+
+  return (
+    <>
+      <label htmlFor="theme">theme</label>
+      <input type="checkbox" id="theme" onChange={handleDarkMode} />
+
+      {/* <div className={`h-30 ${isDark ? `bg-amber-200` : `bg-gray-800`}`}></div> */}
+    </>
+  );
 }
 
 export default Header;
