ea628c114a3b51023027ede707fe103d2429b1ef
Magda Chikhradze <magda292005@gmail.com>
2026-07-23 14:50:07 +0400

    make theme toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,16 +17,18 @@ function Header() {
 
   return (
     <>
-      <label htmlFor="theme">theme</label>
-      <input
-        type="checkbox"
-        id="theme"
-        onChange={handleIsDark}
-        checked={isDark}
-      />
-
-      {/* <div className="h-6 dark:bg-black"></div> */}
-      {/* <div className={`h-30 ${isDark ? `bg-amber-200` : `bg-gray-800`}`}></div> */}
+      <header className="bg-[#6771E3]">
+        <label class="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            className="sr-only peer"
+            onChange={handleIsDark}
+            checked={isDark}
+          />
+          <div className="checkbox-bg relative w-9 h-5  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[#5964E0] after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></div>
+          <span className="select-none ms-3 text-sm font-medium text-heading"></span>
+        </label>
+      </header>
     </>
   );
 }
