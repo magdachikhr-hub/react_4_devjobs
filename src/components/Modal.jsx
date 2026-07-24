475d7d959abd4792e0593027f83f298174bed825
Magda Chikhradze <magda292005@gmail.com>
2026-07-24 15:17:57 +0400

    refactor: simplify modal click handling with stopPropagation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,15 @@
 import React, { useRef } from "react";
 
 function Modal({ close }) {
-  const modalElement = useRef(null);
-  console.log(modalElement.current);
+  // const modalElement = useRef(null);
+  // console.log(modalElement.current);
 
   const handleClosing = (e) => {
-    console.log(e.target);
+    // console.log(e.target);
 
-    if (e.target !== modalElement.current) {
-      close();
-    }
+    // if (e.target !== modalElement.current) {
+    close();
+    // }
   };
 
   return (
@@ -18,7 +18,12 @@ function Modal({ close }) {
         className="absolute bg-[#7a7b7ccb] inset-0 flex justify-center items-center"
         onClick={handleClosing}
       >
-        <div className="bg-white h-54.25 w-81.75" ref={modalElement}></div>
+        <div
+          className="bg-white h-54.25 w-81.75"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+        ></div>
       </div>
     </>
   );
